dea0c119a1578fda92c8fcb7e1f3e335609c3126
EtienneSimoneau <etiennesimoneau@hotmail.fr>
2026-05-21 17:26:56 -0400

    Add SEO improvements

--- web-app/astro.config.mjs
+++ web-app/astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sitemap()],
   output: 'static',
-  site: 'https://renovationjds.ca'
+  site: 'https://constructionrenovationjds.ca'
 });
